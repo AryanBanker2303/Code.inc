61abecec2190354e9e8fccb60813cbcc3af03e28
AryanBanker2303 <60775536+AryanBanker2303@users.noreply.github.com>
2020-03-05 17:34:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,32 @@
-var one,oneLeft,oneRight,oneTop;
 
-function setup() {
-  createCanvas(800,400);
- 
- one = createSprite(400, 300, 500, 200);
+var r = 0;
+var g = 50;
+var b=255;
 
- oneLeft=  createSprite(100,350,100,250);
- oneLeft.shapeColor= rgb(100,100,100);
+// INITIALIZE ALL YOUR VARIABLES
+// Ideally you would create 3 variables. red blue green or r, g, b
 
- oneRight=  createSprite(700,350,100,250);
- oneRight.shapeColor= rgb(150,150,150);
 
- oneTop = createSprite(200,250);
+function setup(){
+
+  createCanvas(1200,400);
 }
 
-function draw() {
-  //background(random(0,255),random(0,255),random(0,255));  
-  background("white");
-  //graphics.drawPolygon(new int[] {10, 20, 30}, new int[] {100, 20, 100}, 3);
-  
-  drawSprites();
+
+function draw(){
+r = map(mouseX,0,1200,0,255);
+g= map(mouseX,0,1200,0,550);
+b = map(mouseX,0,1200,0,20);
+
+background(r,g,b);
+ellipse(mouseX,200,20,20,20,20,20);
+  // change the value of Green based on the mouse movement about the X axis
+  // change the value of Blue based on the mouse movement about the X axis
+
+  // Use the map() function to do so. 
+
+  // Pass the values to the background() function you have learnt previously.
+
+  // Create an ellipse that will move around with you mouse about the X axis.
+  // Remember to fill the canvas with white paint before creating the ellipse.
 }
