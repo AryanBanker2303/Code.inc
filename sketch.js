d5586780bb7871808ba13ae4313acb71c13309d2
AryanBanker2303 <60775536+AryanBanker2303@users.noreply.github.com>
2020-05-04 17:07:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,71 @@
-
-var r = 0;
-var g = 50;
-var b=255;
-
-// INITIALIZE ALL YOUR VARIABLES
-// Ideally you would create 3 variables. red blue green or r, g, b
-
+var player, bulletGroup;
 
 function setup(){
-
-  createCanvas(1200,400);
+createCanvas(1500,700);
+player = createSprite(width/2, height/2,20,20);
+player.shapeColor = "green"
 }
-
 
 function draw(){
-r = map(mouseX,0,1200,0,255);
-g= map(mouseX,0,1200,0,550);
-b = map(mouseX,0,1200,0,20);
-
-background(r,g,b);
-ellipse(mouseX,200,20,20,20,20,20);
-  // change the value of Green based on the mouse movement about the X axis
-  // change the value of Blue based on the mouse movement about the X axis
-
-  // Use the map() function to do so. 
-
-  // Pass the values to the background() function you have learnt previously.
-
-  // Create an ellipse that will move around with you mouse about the X axis.
-  // Remember to fill the canvas with white paint before creating the ellipse.
+background("lime");
+if(frameCount % 50  === 0){
+spawnEnemiesTop();
+spawnEnemiesLeft();
+spawnEnemiesBottom();
+spawnEnemiesRight();
 }
+if(keyWentDown(UP_ARROW)){
+spawnBullets();
+}
+if(keyDown(RIGHT_ARROW)){
+  player.rotation = player.rotation + 5;
+}
+if(keyDown(LEFT_ARROW)){
+  player.rotation = player.rotation -5;
+}
+
+if(keyDown("w")){
+player.velocityY = -4;
+}
+
+if(keyDown("s")){
+  player.velocityY = 4;
+  }
+  
+if(keyDown("a")){
+  player.velocityX = -4;
+  }
+  
+if(keyDown("d")){
+  player.velocityX = 4;
+  }
+drawSprites();
+}
+
+function spawnEnemiesTop(){
+var enemy = createSprite(random(0,1500),0,random(20,40),random(20,40));
+enemy.shapeColor = "red";
+enemy.velocityY = 10;
+}
+function spawnEnemiesLeft(){
+  var enemy = createSprite(0,random(0,700),random(20,40),random(20,40));
+  enemy.shapeColor = "violet";
+  enemy.velocityX = 10;
+  }``
+  function spawnEnemiesBottom(){
+    var enemy = createSprite(random(0,1500),700,random(20,40),random(20,40));
+    enemy.shapeColor = "orange";
+    enemy.velocityY = -10;
+    }
+    function spawnEnemiesRight(){
+      var enemy = createSprite(1500,random(0,700),random(20,40),random(20,40));
+      enemy.shapeColor = "blue";
+      enemy.velocityX = -10;
+      }
+
+function spawnBullets(){
+var bullet  = createSprite(player.x,player.y,10,30);
+bullet.shapeColor = rgb(random(0,255),random(0,255),random(0,255));
+bullet.velocityY = random(-20,20);
+bullet.velocityX = random(-20,20);
+}      
